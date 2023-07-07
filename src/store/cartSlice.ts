fb04d950f1fe52b55aff8a9578e79f9d8d404fd1
Caio Quintas <caioquintassantiago@gmail.com>
2023-07-07 09:51:08 -0300

    fix: cart initial state

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,83 +1,79 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {CartProduct} from "../../types/product";
-import {RootState} from "@/store/store";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+import { CartProduct } from "../../types/product";
+import { RootState } from "@/store/store";
 
 type Totals = {
-    subtotal: number
-    total: number
-}
+  subtotal: number;
+  total: number;
+};
 
 type CartState = {
-    products: CartProduct[]
-    totals: Totals
+  products: CartProduct[];
+  totals: Totals;
 };
 
 const initialState: CartState = {
-    products: [
-        {
-            id: "1",
-            name: "Product 1",
-            price: 100,
-            quantity: 1,
-            image: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-        }
-    ],
-    totals: {
-        subtotal: 100,
-        total: 100,
-    }
+  products: [],
+  totals: {
+    subtotal: 0,
+    total: 0,
+  },
 };
 
-
 export const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        reset: () => initialState,
-        increment: (state, action: PayloadAction<CartProduct>) => {
-            const foundIndex = state.products.findIndex(product => product.id === action.payload.id);
-            const total = state.products.reduce((acc, product) => acc + product.price * product.quantity, 0);
+  name: "cart",
+  initialState,
+  reducers: {
+    reset: () => initialState,
+    increment: (state, action: PayloadAction<CartProduct>) => {
+      const foundIndex = state.products.findIndex(
+        (product) => product.id === action.payload.id
+      );
+      const total = state.products.reduce(
+        (acc, product) => acc + product.price * product.quantity,
+        0
+      );
 
-            if (foundIndex === -1) {
-                state.products.push(action.payload);
-                state.totals.subtotal = total + action.payload.price
-                state.totals.total = total + action.payload.price
-            } else {
-                state.totals.subtotal = total + action.payload.price
-                state.totals.total = total + action.payload.price
-                state.products[foundIndex].quantity += action.payload.quantity;
-            }
-        },
-        remove: (state, action: PayloadAction<CartProduct>) => {
-            const foundIndex = state.products.findIndex(product => product.id === action.payload.id);
-
-            if (foundIndex !== -1) {
-                const price = state.products[foundIndex].price;
-                state.products.splice(foundIndex, 1);
-                state.totals.total -= price * action.payload.quantity;
-                state.totals.subtotal -= price * action.payload.quantity;
-            }
-        },
-
-        decrement: (state, action: PayloadAction<CartProduct>) => {
-            const foundIndex = state.products.findIndex(product => product.id === action.payload.id);
-
-            if (foundIndex !== -1) {
-                const price = state.products[foundIndex].price;
-                state.products[foundIndex].quantity -= 1
-                state.totals.total -= price;
-                state.totals.subtotal -= price;
-            }
-        },
+      if (foundIndex === -1) {
+        state.products.push(action.payload);
+        state.totals.subtotal = total + action.payload.price;
+        state.totals.total = total + action.payload.price;
+      } else {
+        state.totals.subtotal = total + action.payload.price;
+        state.totals.total = total + action.payload.price;
+        state.products[foundIndex].quantity += action.payload.quantity;
+      }
     },
+    remove: (state, action: PayloadAction<CartProduct>) => {
+      const foundIndex = state.products.findIndex(
+        (product) => product.id === action.payload.id
+      );
+
+      if (foundIndex !== -1) {
+        const price = state.products[foundIndex].price;
+        state.products.splice(foundIndex, 1);
+        state.totals.total -= price * action.payload.quantity;
+        state.totals.subtotal -= price * action.payload.quantity;
+      }
+    },
+
+    decrement: (state, action: PayloadAction<CartProduct>) => {
+      const foundIndex = state.products.findIndex(
+        (product) => product.id === action.payload.id
+      );
+
+      if (foundIndex !== -1) {
+        const price = state.products[foundIndex].price;
+        state.products[foundIndex].quantity -= 1;
+        state.totals.total -= price;
+        state.totals.subtotal -= price;
+      }
+    },
+  },
 });
 
-export const {
-    increment,
-    decrement,
-    remove,
-    reset,
-} = cartSlice.actions;
+export const { increment, decrement, remove, reset } = cartSlice.actions;
 
 export const selectProducts = (state: RootState) => state.products;
 export const selectTotals = (state: RootState) => state.totals;
